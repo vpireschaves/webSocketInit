8c94858a8fb9ace9d4f2059863147b7722b4e363
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-27 20:44:35 -0300

    aplicação v0 com o webSocket

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,12 +1,25 @@
-export default function socketInit(io){
-    io.on("connection", (socket) => {
-        console.log(`cliente ${socket.handshake.query.nome} se conectou`);
-        socket.on("disconnect", (msg) => {
-            console.log(`cliente ${socket.id} se desconectou`);
-        })
+import { Server } from 'socket.io'
+import ChatContoller from '../controllers/chatController.js';
 
-        socket.on("enviarMsg", (msg) => {
-            io.emit("enviarMsg", msg);
-        })
-    })
+export default function socketInit(io) {
+    let chatController = new ChatContoller();
+    io.on('connection', (socket) => {
+      let codSala = socket.handshake.query.codSala;
+      let nome = socket.handshake.query.nome;
+      socket.join(codSala);
+      io.to(codSala).emit('entrar', {participante: nome});
+      socket.on('disconnect', () => {          
+          chatController.remover(nome, codSala);
+          io.to(codSala).emit('sair', {saiu: true, qtde: socket.rooms.size, participante: nome});
+        });
+  
+      socket.on('enviarMensagem', (msg) => {
+        io.to(msg.codSala).emit('enviarMensagem', msg);
+      });
+
+      socket.on('sair', (msg) => {
+        io.to(msg.codSala).emit('sair', msg);
+      });
+
+    });
 }
