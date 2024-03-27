830b3c1252b7bef7ccdc4af413066de3ea2c0e28
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-27 20:35:38 -0300

    preenchimento do chatSocket e alterações no sala.html

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,5 +1,12 @@
 export default function socketInit(io){
     io.on("connection", (socket) => {
-        
+        console.log(`cliente ${socket.handshake.query.nome} se conectou`);
+        socket.on("disconnect", (msg) => {
+            console.log(`cliente ${socket.id} se desconectou`);
+        })
+
+        socket.on("enviarMsg", (msg) => {
+            io.emit("enviarMsg", msg);
+        })
     })
 }
